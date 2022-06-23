6830b03410213defa41681248c19d0a9d6905927
Adam Pagels <pagels@hotmail.ca>
2022-06-23 18:30:03 +0200

    Update name

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="app">
       <h1 className="app-page-header">Top 15 Crypto List</h1>
-      <h2 className="app-page-subheader">Technical Test for TradesNest</h2>
+      <h2 className="app-page-subheader">Technical Test for Tradesnest</h2>
       <CryptoList />
     </div>
   );
