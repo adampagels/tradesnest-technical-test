76dd2c4ff0933050b4df9672f2d3687171b7cac8
Adam Pagels <pagels@hotmail.ca>
2022-06-23 14:41:58 +0200

    Render modal and pass function to both modal and card components

--- src/components/CryptoList/CryptoList.tsx
+++ src/components/CryptoList/CryptoList.tsx
@@ -1,18 +1,31 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import CryptoCard from "../CryptoCard/CryptoCard";
 import useFetch from "../../hooks/UseFetch";
+import CryptoModal from "../CryptoModal/CryptoModal";
 
 const CryptoList: FC<{}> = () => {
+  const [clickedCoin, setClickedCoin] = useState<any>(null);
+
   const APIResponse = useFetch(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=15&page=1&sparkline=false"
   );
   const { data } = APIResponse;
+
   return (
     <>
       {data instanceof Array &&
         data.map((coin: any) => {
-          return <CryptoCard key={coin.id} coinData={coin} />;
+          return (
+            <CryptoCard
+              key={coin.id}
+              coinData={coin}
+              setClickedCoin={setClickedCoin}
+            />
+          );
         })}
+      {clickedCoin && (
+        <CryptoModal coinData={clickedCoin} setClickedCoin={setClickedCoin} />
+      )}
     </>
   );
 };
