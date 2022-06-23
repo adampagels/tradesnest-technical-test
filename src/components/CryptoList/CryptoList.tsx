46c7c2ee820761888dbf668a486ea605f087e27e
Adam Pagels <pagels@hotmail.ca>
2022-06-23 16:33:53 +0200

    Add function to remove a coin from the list

--- src/components/CryptoList/CryptoList.tsx
+++ src/components/CryptoList/CryptoList.tsx
@@ -1,28 +1,47 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import CryptoCard from "../CryptoCard/CryptoCard";
 import useFetch from "../../hooks/UseFetch";
 import CryptoModal from "../CryptoModal/CryptoModal";
 
 const CryptoList: FC<{}> = () => {
   const [clickedCoin, setClickedCoin] = useState<any>(null);
+  const [cryptoList, setCryptoList] = useState<any>(null);
 
   const APIResponse = useFetch(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=15&page=1&sparkline=false"
   );
   const { data } = APIResponse;
 
+  const removeCrypto = (e: any, coin: any) => {
+    e.stopPropagation();
+    {
+      const filteredArray =
+        cryptoList instanceof Array &&
+        cryptoList.filter((x: any) => {
+          return x !== coin;
+        });
+      setCryptoList(filteredArray);
+    }
+  };
+
+  useEffect(() => {
+    setCryptoList(data);
+  }, [data]);
+
   return (
     <>
-      {data instanceof Array &&
-        data.map((coin: any) => {
+      {cryptoList instanceof Array &&
+        cryptoList.map((coin: any) => {
           return (
             <CryptoCard
               key={coin.id}
               coinData={coin}
               setClickedCoin={setClickedCoin}
+              removeCrypto={removeCrypto}
             />
           );
         })}
+
       {clickedCoin && (
         <CryptoModal coinData={clickedCoin} setClickedCoin={setClickedCoin} />
       )}
