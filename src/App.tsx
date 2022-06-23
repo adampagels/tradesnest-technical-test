1f6452ac1eaedc41d8c304f04c17acf64b62d3b6
Adam Pagels <pagels@hotmail.ca>
2022-06-23 16:37:16 +0200

    Remove starter code and add header and subheader

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import CryptoList from "./components/CryptoList/CryptoList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1 className="app-page-header">Top 15 Crypto List</h1>
+      <h2 className="app-page-subheader">Technical Test for TradesNest</h2>
+      <CryptoList />
     </div>
   );
 }
